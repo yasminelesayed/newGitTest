1ba4b4aba676408c2a78c2f7ed1e4160819f9b5f
Yasmine Elsayed <yasmine.elsayed@hindawi.local>
2018-04-29 18:42:34 +0200

    this is yasmine part

---
+++ GraphQLFirstSample/GraphQLFirstSample/MySchema.cs
@@ -0,0 +1,37 @@
+﻿using GraphQL.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphQLFirstSample
+{
+    public class MySchema
+    {
+        public GraphQL<MyContext> GraphQL;
+         public MySchema()
+      {
+          var dbContext = new MyContext();
+          dbContext.teachers = new List<Models.Teacher>{
+         new Models.Teacher ()
+         {
+             Id=1,
+             Name="maha"
+         },
+          new Models.Teacher ()
+         {
+             Id=2,
+             Name="yasmine"
+         }
+         };
+
+          var schema = GraphQL<MyContext>.CreateDefaultSchema(() => dbContext);
+            schema.AddType<GraphQLFirstSample.Models.Teacher>().AddAllFields();
+
+            schema.AddListField("teachers", (db) => db.teachers.AsQueryable());
+            schema.Complete();
+            GraphQL = new GraphQL<MyContext>(schema);
+      }
+     
+    }
+}
